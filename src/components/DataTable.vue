572ada7b88f5739a71f7a283ced644ec60fb3527
Rahul Dutt Sharma <r.d.sharma.x@gmail.com>
2023-09-03 18:03:38 +0530

    removed unused files

--- src/components/DataTable.vue
+++ src/components/DataTable.vue
@@ -1,15 +1,28 @@
 <template>
-  <v-data-table-virtual
-    :headers="headers"
-    :items="resultData"
-    class="elevation-1"
-    height="400"
-    :item-value=pk
-    :fixed-header=true
-  ></v-data-table-virtual>
+  <div class="table-wrapper">
+    <p class="px-5 mt-2 bg-grey-lighten-4">Total results (20)</p>
+    <v-data-table-virtual
+      :headers="headers"
+      :items="resultData"
+      class="elevation-1"
+      height="400"
+      :item-value=pk
+      :fixed-header=true
+    ></v-data-table-virtual>
+  </div>
 </template>
 <script setup>
 import { useDataSources } from '@/composables/dataSources'
 
 const { pk, resultData, headers } = useDataSources()
 </script>
+<style scoped>
+  .table-wrapper {
+    max-width: 100%;
+    overflow: hidden;
+    height: 100%;
+  }
+  .bg-grey-lighten-4 {
+    font-size: 12px;
+  }
+</style>
--- src/layouts/default/TableSection.vue
+++ src/layouts/default/TableSection.vue
@@ -4,19 +4,82 @@
       <v-tab value="result">Result</v-tab>
       <v-tab value="schema">Schema</v-tab>
       <v-tab value="charts">Charts</v-tab>
-      <v-btn variant="plain" rounded="0" height="100%">
-        Share
-        <v-dialog v-model="dialog" activator="parent" width="auto">
-          <v-card>
-            <v-form>
-              <v-text-field :rules="rules" label="Enter Comma separated email ids"></v-text-field>
-            </v-form>
-            <v-card-actions>
-              <v-btn color="primary" block @click="dialog = false">Close Dialog</v-btn>
-            </v-card-actions>
-          </v-card>
-        </v-dialog>
-      </v-btn>
+             <v-btn
+          variant="plain"
+          rounded="0"
+          height="100%"
+        >
+          Share
+          <v-dialog v-model="dialog" max-width="500px" activator="parent">
+            <v-card>
+              <v-card-title>
+                Sharable Link to this table
+              </v-card-title>
+              <v-card-text>
+                <div>
+                  <v-container>
+                  <v-row align="center" class="bg-grey-lighten-3 py-2 mt-1 mb-2">
+                    <v-col>
+                      <p>{{ dummyLink }}</p>
+                    </v-col>
+                    <v-col cols="auto">
+                      <v-btn flat icon @click="copyLink">
+                        <v-icon>
+                            mdi-content-copy
+                          </v-icon>
+                        <v-tooltip
+                          activator="parent"
+                          location="top"
+                        >Copy</v-tooltip>
+                      </v-btn>
+                    </v-col>
+                  </v-row>
+                  </v-container>
+                </div>
+                <p>Add comma separated emails to share this link to:</p>
+                <v-text-field
+                  v-model="emailList"
+                  label="Emails"
+                  outlined
+                  multi-line
+                ></v-text-field>
+              </v-card-text>
+              <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn @click="cancel" color="error">
+                  Cancel
+                </v-btn>
+                <v-btn color="primary">
+                  Submit
+                </v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-dialog>
+        </v-btn>
+        <v-menu>
+            <template v-slot:activator="{ props }">
+              <v-btn
+                variant="plain"
+                rounded="0"
+                height="100%"
+                v-bind="props"
+              >
+                Save
+                <v-icon end>
+                  mdi-menu-down
+                </v-icon>
+              </v-btn>
+            </template>
+
+            <v-list class="bg-grey-lighten-3">
+              <v-list-item
+                v-for="item in saveOptions"
+                :key="item"
+              >
+                {{ item }}
+              </v-list-item>
+            </v-list>
+          </v-menu>
       <v-menu>
         <template v-slot:activator="{ props }">
           <v-btn variant="plain" rounded="0" height="100%" v-bind="props">
@@ -61,7 +124,15 @@ const { resultData, schema, selectedTable } = useDataSources()
 const tab = ref('')
 const dialog = ref(false)
 const exportOptions = ['csv', 'excel']
+const saveOptions = ref(['Save as new table', 'Save to other database'])
 const rules = ref([])
+const dummyLink = ref('https://example.com')
+const emailList = ref('')
+
+const cancel = () => {
+  dialog.value = false; 
+  emailList.value = '';
+};
 
 function exportTo(exportType) {
   if (exportType === 'excel') {
@@ -110,6 +181,7 @@ function exportExcel() {
 
   xlsx(data, settings)
 }
-
+function copyLink() {
+  console.log('link copied')
+}
 </script>
-
